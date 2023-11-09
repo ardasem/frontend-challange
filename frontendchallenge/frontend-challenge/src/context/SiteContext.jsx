b3fddff056523aa947781cf3dfd2b3a3612384ac
ardasem <73624089+ardasem@users.noreply.github.com>
2023-11-09 09:22:01 +0300

    commited

---
+++ frontendchallenge/frontend-challenge/src/context/SiteContext.jsx
@@ -0,0 +1,19 @@
+import { createContext,useState } from "react";
+import data from './data.js' 
+
+export const SiteContext = createContext();
+
+export const SiteContextProvider = ({children})=>{
+    const [lang, setLang] = useState("tr");
+    const [theme, setTheme] = useState("light");
+    const [siteTexts,setSiteTexts] = useState(data)
+
+    const texts = siteTexts[lang]
+    
+
+    return (
+        <SiteContext.Provider value = {{lang: lang, theme: theme, setTheme, setLang, texts}}>
+            {children}
+        </SiteContext.Provider>
+    )
+}
---
+++ frontendchallenge/frontend-challenge/src/data.js
@@ -0,0 +1,89 @@
+export const data = {
+ en: {
+    skills: ["react", "js", "redux", "node", "vscode", "figma"],
+    linkedinUrl: "",
+    githubUrl: "",
+    basicInfo: {
+      birthDate: "288.11.1995",
+      cityOfResidence: "Istanbul",
+      academicStatus: "BS Mechatronics Engineer, Sabanci University",
+      role: "Front-end Web Developer, UI Developer",
+      aboutMe: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Minima nam incidunt porro doloribus voluptatum dolorem excepturi 
+        voluptas aliquam voluptatibus natus, quo delectus dolorum tempora 
+        itaque, deleniti alias. Ullam, nesciunt magni`,
+        email: 'arda.sem@gmail.com'
+    },
+    projects: [
+      {
+        id: "1",
+        projectName: "",
+        tags: ["react", "redux", "tailwind"],
+        projectInfo: "",
+        projectUrl: "",
+        projectIcon: "",
+      },
+      {
+        id: "2",
+        projectName: "",
+        tags: ["react", "redux", "tailwind"],
+        projectInfo: "",
+        projectUrl: "",
+        projectIcon: "",
+      },
+      {
+        id: "3",
+        projectName: "",
+        tags: ["react", "redux", "tailwind"],
+        projectInfo: "",
+        projectUrl: "",
+        projectIcon: "",
+      },
+    ],
+  },
+ tr: {
+    skills: ["react", "js", "redux", "node", "vscode", "figma"],
+    linkedinUrl: "",
+    githubUrl: "",
+    basicInfo: {
+      birthDate: "288.11.1995",
+      cityOfResidence: "Istanbul",
+      academicStatus: "BS Mekatronik Muhendisligi, Sabanci Universitesi",
+      role: "Front-end Web Gelistirici, UI Gelistirici",
+      aboutMe: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Minima nam incidunt porro doloribus voluptatum dolorem excepturi 
+        voluptas aliquam voluptatibus natus, quo delectus dolorum tempora 
+        itaque, deleniti alias. Ullam, nesciunt magni`,
+        email: 'arda.sem@gmail.com'
+    },
+    projects: [
+      {
+        id: "1",
+        projectName: "",
+        tags: ["react", "redux", "tailwind"],
+        projectInfo: "",
+        projectUrl: "",
+        projectIcon: "",
+      },
+      {
+        id: "2",
+        projectName: "",
+        tags: ["react", "redux", "tailwind"],
+        projectInfo: "",
+        projectUrl: "",
+        projectIcon: "",
+      },
+      {
+        id: "3",
+        projectName: "",
+        tags: ["react", "redux", "tailwind"],
+        projectInfo: "",
+        projectUrl: "",
+        projectIcon: "",
+      },
+    ],
+  },
+
+}
+ 
+;
